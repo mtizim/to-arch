9db7f15e7b8c8b352ad9d3bb94a9b1cbffc75a71
YTG123 <ytg1234@pm.me>
2021-09-10 15:17:16 +0300

    postrun.sh changes; see details
    Use grep -q instead of checking output
    
    Use tr to check the reboot prompt answer case-insensitively
    
    Use read -r for reboot
    
    Get rid of unnecessary single-command if

--- src/postrun.sh
+++ src/postrun.sh
@@ -1,11 +1,12 @@
- #KDE Plasma theme switch to default(User mode)
-if [ "$(pacman -Qq | grep plasma-desktop)" ]; then
+# KDE Plasma theme switch to default (user mode)
+if pacman -Qq | grep -q plasma-desktop; then
 	/usr/lib/plasma-changeicons breeze-dark 2>/dev/null
 	lookandfeeltool --apply "org.kde.breezedark.desktop" 2>/dev/null
 fi
+
 chsh -s "$(which bash)"
+
 printf "Would you like to reboot? Make sure you have read the above carefully! (y/N)"
-read reboot
-if [ "${reboot}" == "y" ]; then
-	reboot
-fi
+read -r reboot
+# If we're already using Bash (#!/usr/bin/env bash), why not make use of its neat features
+[ "$(tr '[:upper:]' '[:lower:]' <<< "$reboot")" = "y" ] && reboot
