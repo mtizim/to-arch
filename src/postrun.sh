1e0a7daf2ef0c3218de85109fe591d028b8ec60f
sugkiy <sugkiy@protonmail.com>
2021-09-10 23:19:22 +0900

    Update postrun.sh

--- src/postrun.sh
+++ src/postrun.sh
@@ -3,7 +3,7 @@ if [ "$(pacman -Qq | grep plasma-desktop)" ]; then
 	/usr/lib/plasma-changeicons breeze-dark 2>/dev/null
 	lookandfeeltool --apply "org.kde.breezedark.desktop" 2>/dev/null
 fi
-chsh -s "$(which bash)"
+chsh -s "/bin/bash"
 printf "Would you like to reboot? Make sure you have read the above carefully! (y/N)"
 read reboot
 if [ "${reboot}" == "y" ]; then
