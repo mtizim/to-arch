8cd136c14d386658068fe1c0de0ecdecdd531b85
YTG123 <ytg1234@pm.me>
2021-09-10 15:58:53 +0300

    Cleanup for src/convert.sh; see details
    - Spaces after comment `#`s
    - Replace decently short single-command-body if statements with the `&&` operator
    - Replace repeated calls to `pacman -Qq` and `grep` with a function
    - `grep` quietly and check exit code instead of checking the output
    - Move the mirrorlist backup to an auto-generated temporary directory
    - Instead of two `cd` calls, use a subshell
    - Use `case` instead of `if` where applicable
    - Remove duplicate line from both branches of if (GRUB theme)
    - Remove single-quote escapes inside double quotes
    
    The git diff is completely borked so I had to do this all in one commit

--- src/convert.sh
+++ src/convert.sh
@@ -1,171 +1,143 @@
+# Run `pacman -Qq` and grep a pattern quietly
+grepPacmanQuery() { # $1 - Pattern to grep for in the output of `pacman -Qq`
+	pacman -Qq | grep "$1" -q
+}
+
+# Remove a package if it matched `pacman -Qq`
+removeIfMatched() { # $1 - Pattern
+	grepPacmanQuery "$1" && pacman -Rsdd "$1" --noconfirm
+	true
+}
+
+# Temporary directory to store all our stuff in
+tmp_dir="$(mktemp -d)"
+
 pacman -Syy neofetch --noconfirm
 neofetch
 printf "This is your current distro state.\n"
-if [ "$(pacman -Qq | grep pamac)" ]; then
-	pacman -Qq | grep pamac | xargs pacman -Rdd --noconfirm
-fi
 
-if [ "$(pacman -Qq | grep manjaro-application-utility)" ]; then
-	yes | pacman -Rcnsdd manjaro-application-utility --noconfirm
-fi
+grepPacmanQuery pamac && pacman -Qq | grep pamac | xargs pacman -Rdd --noconfirm
+grepPacmanQuery manjaro-application-utility && pacman -Rcnsdd manjaro-application-utility
+removeIfMatched matray
+removeIfMatched manjaro-release
+removeIfMatched bashrc-manjaro
+removeIfMatched manjaro-keyring
 
-if [ "$(pacman -Qq | grep matray)" ]; then
-	yes | pacman -Rsdd matray --noconfirm
-fi
+(
+	cd /etc/pacman.d
+	rm mirrorlist
+	# Get mirrorlist
+	curl -o mirrorlist -sL 'https://archlinux.org/mirrorlist/?country=all&protocol=http&protocol=https&ip_version=4&ip_version=6'
 
-if [ "$(pacman -Qq | grep manjaro-release)" ]; then
-	yes | pacman -Rsdd manjaro-release
-fi
+	[ -f /etc/pacman.d/mirrorlist.pacnew ] && rm /etc/pacman.d/mirrorlist.pacnew
+	[ -f /etc/pacman.conf.pacnew ] && rm /etc/pacman.conf.pacnew
+	
+	# Delete crappy unrecognized pacman option by Manjaro
+	sed -i '/SyncFirst/d' /etc/pacman.conf
+	sed -i '/HoldPkg/d' /etc/pacman.conf
+	
+	# Use $VISUAL instead?
+	printf "==> Uncomment mirrors from your country.\nPress 1 for Nano, 2 for vim, or any other key for your default \$EDITOR.\n"
+	read -rn 1 whateditor
+	case "$whateditor" in
+		"1") nano /etc/pacman.d/mirrorlist ;;
+		"2") vim /etc/pacman.d/mirrorlsit ;;
+		*) $EDITOR /etc/pacman.d/mirrorlist ;;
+	esac
+	
+	# Backup just in case
+	cp /etc/pacman.d/mirrorlist "${tmp_dir}/mirrorlist"
+)
 
-if [ "$(pacman -Qq | grep bashrc-manjaro)" ]; then
-	yes | pacman -Rsdd bashrc-manjaro
-fi
+# I have seen pacui and bmenu in some editions as a dependency
+# I have go goddamn idea why this shows up in cat
+grepPacmanQuery pacui && pacman -Qq | grep pacui | xargs pacman -Rdd --noconfirm
+pacman -Qq pacui &>/dev/null && pacman -Qq | grep pacui | xargs pacman -Rdd --noconfirm
+pacman -Qq bmenu &>/dev/null && pacman -Qq | grep bmenu | xargs pacman -Rdd --noconfirm
+pacman -Qq pacman-mirrors &>/dev/null && pacman -Qq | grep pacman-mirrors | xargs pacman -Rdd --noconfirm
 
-if [ "$(pacman -Qq | grep manjaro-keyring)" ]; then
-	yes | pacman -Rsdd manjaro-keyring
-fi
-
-#Some more seamless stuff
-p_w_d="$(directory)"
-
-cd /etc/pacman.d
-rm mirrorlist
-#Get mirrorlist
-curl -o mirrorlist -sL 'https://archlinux.org/mirrorlist/?country=all&protocol=http&protocol=https&ip_version=4&ip_version=6'
-
-if [ -f /etc/pacman.d/mirrorlist.pacnew ]; then
-	rm /etc/pacman.d/mirrorlist.pacnew
-fi
-
-if [ -f /etc/pacman.conf.pacnew ]; then
-	mv /etc/pacman.conf.pacnew /etc/pacman.conf
-fi
-
-#Delete crappy unrecognized pacman option by manjaro
-sed -i '/SyncFirst/d' /etc/pacman.conf
-sed -i '/HoldPkg/d' /etc/pacman.conf
-
-printf "==> Uncomment mirrors from your country.\nPress 1 for Nano, 2 for vim, or any other key for your default \$EDITOR.\n"
-read -n 1 whateditor
-if [ "${whateditor}" == "1" ]; then
-	nano /etc/pacman.d/mirrorlist
-elif [ "${whateditor}" == "2" ]; then
-	vim /etc/pacman.d/mirrorlist
-else
-	$EDITOR /etc/pacman.d/mirrorlist
-fi
-#backup just in case
-cp /etc/pacman.d/mirrorlist /tmp/mirrorlist
-
-cd "${directory}"
-#I have seen pacui and bmenu in some editions as a dependency
-#I have go goddamn idea why this shows up in cat
-if [ "$(pacman -Qq pacui)" ]; then
-	pacman -Qq | grep pacui | xargs pacman -Rdd --noconfirm
-fi
-
-if [ "$(pacman -Qq bmenu)" ]; then
-	pacman -Qq | grep bmenu | xargs pacman -Rdd --noconfirm
-fi
-
-if [ "$(pacman -Qq pacman-mirrors)" ]; then
-	pacman -Qq | grep pacman-mirrors | xargs pacman -Rdd --noconfirm
-fi
-
-#Get pacman and mirrorlist and lsb_release from website, not mirrors
+# Get pacman, mirrorlist and lsb_release from website, not mirrors
 pacman -U https://www.archlinux.org/packages/core/x86_64/pacman/download/ https://www.archlinux.org/packages/core/any/pacman-mirrorlist/download/ https://www.archlinux.org/packages/community/any/lsb-release/download/ --noconfirm
 
-\mv -f /tmp/mirrorlist /etc/pacman.d/mirrorlist
-#Do it again, because conf gets resetted
+\mv -f "${tmp_dir}/mirrorlist" /etc/pacman.d/mirrorlist
+# Do it again, because conf gets reset
 sed -i '/SyncFirst/d' /etc/pacman.conf
 
-if [ -d /sys/firmware/efi ]; then
-	#Deletes the Manjaro UEFI entry. Very dangerous operation if misused, but I tested this multiple times and it was good.
-	efibootmgr -b "$(efibootmgr | grep Manjaro | sed 's/*//' | cut -f 1 -d' ' | sed 's/Boot//')" -B
-fi
+# Deletes the Manjaro UEFI entry. Very dangerous operation if misused, but I tested this multiple times and it was good.
+[ -d /sys/firmware/efi ] && efibootmgr -b "$(efibootmgr | grep Manjaro | sed 's/*//' | cut -f 1 -d' ' | cut -c5-)" -B
 
-#Change grub
+# Change grub
 sed -i '/GRUB_DISTRIBUTOR="Manjaro"/c\GRUB_DISTRIBUTOR="Arch"' /etc/default/grub
 
-# following line enables multilib repository
+# Following line enables multilib repository
 sed -ie 's/#\(\[multilib\]\)/\1/;/\[multilib\]/,/^$/{//!s/^#//;}' /etc/pacman.conf
 
-#Prevent HoldPkg error
+# Prevent HoldPkg error
 sed -i '/HoldPkg/d' /etc/pacman.conf
-#purge manjaro's software
+# Purge Manjaro's software
 pacman -Qq | grep manjaro | xargs pacman -Rdd --noconfirm
-#KDE Plasma
+# KDE Plasma
 pacman -Qq | grep breath | xargs pacman -Rdd --noconfirm
 
-#This should be in front of -Syu to avoid manjaro's linux kernel from updating
-pacman -Qq | grep linux[0-9] | xargs pacman -Rdd --noconfirm
+# This should be in front of -Syu to avoid manjaro's linux kernel from updating
+pacman -Qq | grep 'linux[0-9]' | xargs pacman -Rdd --noconfirm
 
-#-Syyyyyyyyyyuuuuuuuu calms me down
-yes | pacman -Syyuu bash
+# -Syyyyyyyyyyuuuuuuuu calms me down
+pacman -Syyuu bash --noconfirm
 
-#As Linus Torvalds said
+# As Linus Torvalds said
 pacman -Qq | grep mhwd | xargs pacman -Rdd --noconfirm
 
-#Change computer's name if it's manjaro
+# Change computer's name if it's manjaro
 if [ -f /etc/hostname ]; then
 	sed -i '/manjaro/c\Arch' /etc/hostname
 	sed -i '/Manjaro/c\Arch' /etc/hostname
 fi
+
 sed -i '/manjaro/c\Arch' /etc/hosts
 sed -i '/Manjaro/c\Arch' /etc/hosts
 
-#linux-lts is generally more stable(especially for intel graphics, uhd620 seems to have a black screen issue since 5.11)
-printf "What kernel do you want?\nThe LTS kernel tends to be more stable.\nPress 1 for LTS, and 2 for the normal kernel."
-read -n 1 whatkernel
-if [ "${whatkernel}" == "1" ]; then
-	pacman -S linux-lts linux-lts-headers --noconfirm
-elif [ "${whatkernel}" == "2" ]; then
-	pacman -S linux linux-headers --noconfirm
-else
-	pacman -S linux-lts linux-lts-headers --noconfirm
-fi
+# linux-lts is generally more stable (especially for intel graphics, uhd620 seems to have a black screen issue since 5.11)
+printf "What kernel do you want?\nThe LTS kernel tends to be more stable.\nPress 1 for LTS, and 2 for the normal kernel. "
+read -rn 1 whatkernel
 
-#Fück you nvidia
-if [ "$(lspci | grep -i nvidia)" ]; then
+case "$whatkernel" in
+	"2") pacman -S linux linux-headers --noconfirm ;;
+	*) pacman -S linux-lts linux-lts-headers --noconfirm ;;
+esac
+
+# Fück you nvidia
+if lspci | grep -iq nvidia; then
 	pacman -Qq | grep nvidia | xargs pacman -Rdd --noconfirm
 	pacman -S nvidia --noconfirm
 fi
-#Delete line that hides grub. Manjaro devs, do you think that noobs don't even know how to press enter?
+
+# Delete line that hides GRUB. Manjaro devs, do you think that noobs don't even know how to press enter?
 sed -i '/GRUB_TIMEOUT_STYLE=hidden/d' /etc/default/grub
 
-#Changes Manjaro grub theme. Manjaro doesn't have an option to install systemd-boot, Right? I'm just assuming you have a clean install of Manjaro.
+# Changes Manjaro GRUB theme. Manjaro doesn't have an option to install systemd-boot, Right? I'm just assuming you have a clean install of Manjaro.
 curl -fLOs https://github.com/AdisonCavani/distro-grub-themes/releases/download/2.1/ArchLinux.tar
 
-if ! [ -d /boot/grub/themes/archlinux ]; then
-	mkdir /boot/grub/themes/archlinux
-else
-	rm -rf /boot/grub/themes/archlinux
-	mkdir /boot/grub/themes/archlinux
-fi
+[ -d /boot/grub/themes/archlinux ] && rm -rf /boot/grub/themes/archlinux
+mkdir /boot/grub/themes/archlinux
 
 tar -xf ArchLinux.tar -C /boot/grub/themes/archlinux
 sed -i '/GRUB_THEME=/c GRUB_THEME="/boot/grub/themes/archlinux/theme.txt"' /etc/default/grub
 
-#Generate grub stuff
+# Generate GRUB stuff
 grub-mkconfig -o /boot/grub/grub.cfg
-if [ -d /sys/firmware/efi ]; then
-	grub-install
-fi
+[ -d /sys/firmware/efi ] && grub-install
 
-#Locale fix
-#It scared the daylights out of me when I realized gnome-terminal won't start without this part
-if [ -f /etc/locale.conf.pacsave ]; then
-	\mv -f /etc/locale.conf.pacsave /etc/locale.conf
-fi
+# Locale fix
+# It scared the daylights out of me when I realized gnome-terminal won't start without this part
+[ -f /etc/locale.conf.pacsave ] && \mv -f /etc/locale.conf.pacsave /etc/locale.conf
 locale-gen
 
 
-#Deletes Manjaro-Sway repo
-if [ "$(pacman -Qq | grep sway)" ]; then
-	sed -ie '/[manjaro-sway]/,+2d' /etc/pacman.conf
-fi
+# Deletes Manjaro-Sway repo
+grepPacmanQuery sway && sed -ie '/[manjaro-sway]/,+2d' /etc/pacman.conf
 
-#Greeter bg remove(Doesn't really work idk why)
+# Greeter bg remove (Doesn't really work idk why)
 if [ -f /etc/lightdm/unity-greeter.conf ]; then
 	sed -i '/background/d' /etc/lightdm/unity-greeter.conf
 	sed -i '/default-user-image/d' /etc/lightdm/unity-greeter.conf
@@ -177,14 +149,14 @@ if [ -f /etc/lightdm/lightdm-gtk-greeter.conf ]; then
 fi
 
 neofetch
-printf "Now it\'s Arch! Enjoy!\n"
+printf "Now it's Arch! Enjoy!\n"
 printf "There could be some leftover Manjaro backgrounds and themes/settings(especially lightdm),\nso you might have to tweak your desktop environment a bit.\n"
-if [ "$(pacman -Q | grep deepin-desktop-base)" ]; then
-	printf "When you reboot, the theme will be changed to stock white but the font won\'t,\nso change it to dark again and it\'ll be fixed..\nAnd especially on VMs after login everything will be white.\nBlindly press on the middle of the screen and you\'ll be logged in.\n"
-fi
-if [ "$(systemctl list-unit-files | grep enabled | grep sddm)" ]; then
-	printf "You seem to run SDDM.\nMake sure you change the SDDM theme to something else like breeze because the default theme looks horrible!"
+if grepPacmanQuery deepin-desktop-base; then
+	printf "When you reboot, the theme will be changed to stock white but the font won't,\nso change it to dark again and it'll be fixed..\n"
+	printf "And especially on VMs after login everything will be white.\nBlindly press on the middle of the screen and you'll be logged in.\n"
 fi
 
-
+if systemctl list-unit-files | grep enabled | grep -q sddm; then
+	printf "You seem to run SDDM.\nMake sure you change the SDDM theme to something else like breeze because the default theme looks horrible!\n"
+fi
 
