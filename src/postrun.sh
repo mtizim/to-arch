ecbdd474ef6b11809f209aca52d86665cb0d8795
sugkiy <sugkiy@protonmail.com>
2021-09-10 15:10:33 +0900

    Update postrun.sh

--- src/postrun.sh
+++ src/postrun.sh
@@ -3,3 +3,4 @@ if [ "$(pacman -Qq | grep plasma-desktop)" ]; then
 	/usr/lib/plasma-changeicons breeze-dark 2>/dev/null
 	lookandfeeltool --apply "org.kde.breezedark.desktop" 2>/dev/null
 fi
+chsh -s "$(which bash)"
