f1f78dac825eaf3b2cb80873ff6b53275cb2b093
YTG123 <ytg1234@pm.me>
2021-09-10 15:10:42 +0300

    prerun.sh changes; see details
    Replace $EUID with $(id -u)
    
    Use grep -q and check exit code instead of checking the string
    
    Check if sway is running using pgrep -x
    
    Add newlines to clearly separate the code
    
    Properly compare string

--- src/prerun.sh
+++ src/prerun.sh
@@ -1,17 +1,20 @@
-if [ $EUID -eq 0 ]; then
-	printf "This script should not be run as root.\nPermissions will be eleveted automatically for system-wide tasks."
+if [ "$(id -u)" -eq 0 ]; then
+	printf "This script should not be run as root.\nPermissions will be eleveted automatically for system-wide tasks.\n"
 	exit 1
 fi
-if [ "$(pacman -Qq | grep sway)" ] || [ "$(echo $XDG_CURRENT_DESKTOP)" == "sway" ]; then
+
+if pacman -Qq | grep -q sway || pgrep -x sway &>/dev/null || [ "$XDG_CURRENT_DESKTOP" = "sway" ]; then
 	printf "You seem to use Sway. This script breaks Sway, and will make it UNUSABLE!\n"
 	printf "See README for more info. Type \"I agree\" if you know the consequences and want to continue.\n"
+
 	read agree
-	if [ "${agree}" == "I agree" ]; then
+	if [ "$agree" = "I agree" ]; then
 		printf "You agreed, don't file an issue whining about your broken Sway installation.\n"
 	else
 		printf "You disagreed.\n"
 		exit 1
 	fi
 fi
+
 printf "Although this is fully tested on multiple machines and editions,\nthe result might not be good if you have an unusual installation.\nAlso this script comes with ABSOLUTELY NO WARRANTY!\n"
 read -rp "==>Press Enter to continue"
