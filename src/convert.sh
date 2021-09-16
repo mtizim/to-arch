a387c36870e55a0833685f731a221768a06ee65a
sugkiy <sugkiy@protonmail.com>
2021-09-16 16:52:45 +0900

    Update convert.sh

--- src/convert.sh
+++ src/convert.sh
@@ -2,7 +2,7 @@
 # For god's sake people why do you use LUKS. The NSA does not have enough free time to spy on you and try to decrypt your whole disk.
 # A screen password is completely OK for PC uses. The FBI won't try to rip off your hard disk unless you're some kind of unabomber.
 if [ $(sudo fdisk -l 2>/dev/null | grep -i luks) ]; then
-	printf "You seem to use LUKS. This script breaks LUKS. Please don\'t use this."
+	printf "You seem to use LUKS. This script breaks LUKS. Please don\'t use this.\n"
 	exit 1
 fi
 [ $? == 1 ] && exit 1;
