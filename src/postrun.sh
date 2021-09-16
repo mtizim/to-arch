6061448fd5823c214ef2de0f4dab4fff69fbb5e0
sugkiy <sugkiy@protonmail.com>
2021-09-16 16:47:04 +0900

    Update postrun.sh

--- src/postrun.sh
+++ src/postrun.sh
@@ -3,7 +3,11 @@ if pacman -Qq | grep -q plasma-desktop; then
 	/usr/lib/plasma-changeicons breeze-dark 2>/dev/null
 	lookandfeeltool --apply "org.kde.breezedark.desktop" 2>/dev/null
 fi
+# Changes shell since this will remove Manjaro's zsh configs.
+# I use yash btw.
 chsh -s "/bin/bash"
+# Deletes sway config cuz sway edition saves its configs in a packages and sway gets borked after deleteing those.
+# And yes sway will look like plain i3.
 if pacman -Qq | grep -q sway; then
 	workdir="$(pwd)"
 	rm -rf ~/.config/sway
@@ -13,12 +17,16 @@ if pacman -Qq | grep -q sway; then
 	sed -i '/alacritty/c\foot' ~/.config/sway/config
 	sed -i '/Wallpaper/d' ~/.config/sway/config
 	cd "${workdir}"
+	# I know this part is extremely hacky and weird, but chsh doesn't change foots shell.
 	echo "clear;exec bash" >> ~/.zshrc
 fi
+# Some i3 MaNjArO removal.
 if pacman -Qq | grep -q i3; then
 	sed -i '/nitrogen/d' ~/.i3/config 2>/dev/null
 fi
+# Reboot if you want.
 printf "Would you like to reboot? Make sure you have read the above carefully! (y/N)"
 read -r reboot
+#Thanks to YTG1234 for this line. This is a pure bit of genius.
 # If we're already using Bash (#!/usr/bin/env bash), why not make use of its neat features
 [ "$(tr '[:upper:]' '[:lower:]' <<< "$reboot")" = "y" ] && reboot
